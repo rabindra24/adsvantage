4959cf83ca6529500b6e11fa1a208e71643a7ff2
rabin2403 <105305328+rabin2403@users.noreply.github.com>
2024-04-15 09:45:41 +0530

    images adjust

--- constant/images.ts
+++ constant/images.ts
@@ -6,36 +6,36 @@ export interface CustomImage extends Image {
 
 export const images: CustomImage[] = [
   {
-    src: '../public/images/result (1).png',
-    original: '../public/images/result (1).png',
+    src: '/images/result (1).png',
+    original: '/images/result (1).png',
     width: 320,
     height: 174,
-    tags: [
-      { value: "Nature", title: "Nature" },
-      { value: "Flora", title: "Flora" },
-    ],
+    // tags: [
+    //   { value: "Nature", title: "Nature" },
+    //   { value: "Flora", title: "Flora" },
+    // ],
     caption: "After Rain (Jeshu John - designerspics.com)",
   },
   {
-    src: '../public/images/result (1).png',
-    original: '../public/images/result (1).png',
+    src: '/images/result (1).png',
+    original: '/images/result (1).png',
     width: 320,
     height: 212,
-    caption: "Boats (Jeshu John - designerspics.com)",
+    // caption: "Boats (Jeshu John - designerspics.com)",
   },
   {
-    src: '../public/images/result (1).png',
-    original: '../public/images/result (1).png',
+    src: '/images/result (1).png',
+    original: '/images/result (1).png',
     width: 320,
     height: 212,
-    caption: "Color Pencils (Jeshu John - designerspics.com)",
+    // caption: "Color Pencils (Jeshu John - designerspics.com)",
   },
   {
-    src: '../public/images/result (1).png',
-    original: '../public/images/result (1).png',
+    src: '/images/result (1).png',
+    original: '/images/result (1).png',
     width: 320,
     height: 213,
-    caption: "Red Apples with other Red Fruit (foodiesfeed.com)",
+    // caption: "Red Apples with other Red Fruit (foodiesfeed.com)",
   },
  
 ];
